0df737d028c24b1361a79d05f350aff8633ef1fc
Alejandro-Lara <alara01@manhattan.edu>
2020-03-14 18:42:42 -0400

    cleaned up the comments and added another arg check

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import sys
 
-str1 = sys.argv[1]
-str2 = sys.argv[2]
-
-if len(str1) != len(str2):
-    print('false');
+if(len(sys.argv) != 3):
+    print('ERROR: This script requires exactly two arguments')
 else:
+     
+    str1 = sys.argv[1]
+    str2 = sys.argv[2]
 
-    pairs = dict()
+    if len(str1) != len(str2):
+        print('false');
+    else:
 
-    for i in range(len(str1)):
-        if str1[i] not in pairs:
-            pairs[str1[i]] = str2[i] #associate these two characters together
+        pairs = dict()
+        oneToOne = True
+
+        for i in range(len(str1)):
+            if str1[i] not in pairs:
+                pairs[str1[i]] = str2[i] #associate these two characters together
+                
+            elif pairs[str1[i]]!= str2[i]:
+                    #if the character from str1 previously mapped to a different
+                    #character in str2, then it's false
+                    oneToOne = False
+                    break
+    
+        if oneToOne:      
+            print('true')
         else:
-            if pairs[str1[i]]!= str2[i]:
-                #if the same character from str1 maps to multiple and different
-                #characters in str2, then fail
-                print('false')
-                exit()
-            
-    print('true')
+            print('false')
